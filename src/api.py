bc98b05280c357ebb28e4f157c34aa52b24b6cad
Zac Garby <me@zacgarby.co.uk>
2024-11-23 10:06:51 +0000

    api

--- src/api.py
+++ src/api.py
@@ -10,6 +10,27 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+@router.get("/product/search")
+async def product_query(
+    id: str | None = Query(default=None),
+    name: str | None = Query(default=None),
+    taxon: int = Query(default=0),
+    limit: int = Query(default=5),
+    session: Session = Depends(get_session),
+) -> list[Product]:
+    query = select(Product).where(
+        Product.taxonomies.any(Taxonomy.id == taxon) # type: ignore
+    ).limit(limit).order_by(Product.id)
+
+    if id:
+        query = query.where(Product.id == id)
+
+    if name:
+        query = query.where(Product.name.ilike(f"%{name}%")) # type: ignore
+
+    prods = session.exec(query)
+    return list(prods.all())
+
 @router.get("/product/{id}")
 async def product_by_id(
     id: int,
@@ -21,24 +42,6 @@ async def product_by_id(
         return None
 
     return prod
-
-@router.get("/product/search")
-async def product_query(
-    id: str | None = Query(default=None),
-    name: str | None = Query(default=None),
-    limit: int = Query(default=5),
-    session: Session = Depends(get_session),
-) -> list[Product]:
-    query = select(Product).limit(limit).order_by(Product.id)
-
-    if id:
-        query = query.where(Product.id == id)
-
-    if name:
-        query = query.where(Product.name.ilike(f"%{name}%")) # type: ignore
-
-    prods = session.exec(query)
-    return list(prods.all())
 
 @router.get("/taxonomy")
 async def get_taxonomy(
